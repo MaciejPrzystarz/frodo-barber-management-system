f1c4f4258176bf0d8e687394574f61dd077906ed
MaciejPrzystarz <maciej.przystarz7@gmail.com>
2026-01-19 13:06:37 +0100

    Dodanie securityFilterChain oraz konfiguracja profilu dev do działania h2

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .headers(h -> h.frameOptions(f -> f.disable()))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
