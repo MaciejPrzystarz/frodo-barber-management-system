e4f0b761f862cd94f3245d7dcc5a7500cc75a212
MaciejPrzystarz <maciej.przystarz7@gmail.com>
2026-01-19 13:05:32 +0100

    Dodanie klas User, Role oraz danych testowych do ról Admin, Barber, Client.

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String fullName;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO users (email, full_name, password, role) VALUES
+    ('admin@demo.pl',  'Admin Demo',  'Admin123!',  'ADMIN');
+
+INSERT INTO users (email, full_name, password, role) VALUES
+    ('barber@demo.pl', 'Barber Demo', 'Barber123!', 'BARBER');
+
+INSERT INTO users (email, full_name, password, role) VALUES
+                                                         ('client1@demo.pl',  'Klient 01', 'Client123!', 'CLIENT'),
+                                                         ('client2@demo.pl',  'Klient 02', 'Client123!', 'CLIENT'),
+                                                         ('client3@demo.pl',  'Klient 03', 'Client123!', 'CLIENT'),
+                                                         ('client4@demo.pl',  'Klient 04', 'Client123!', 'CLIENT'),
+                                                         ('client5@demo.pl',  'Klient 05', 'Client123!', 'CLIENT'),
+                                                         ('client6@demo.pl',  'Klient 06', 'Client123!', 'CLIENT'),
+                                                         ('client7@demo.pl',  'Klient 07', 'Client123!', 'CLIENT'),
+                                                         ('client8@demo.pl',  'Klient 08', 'Client123!', 'CLIENT'),
+                                                         ('client9@demo.pl',  'Klient 09', 'Client123!', 'CLIENT'),
+                                                         ('client10@demo.pl', 'Klient 10', 'Client123!', 'CLIENT');
+
+-- HASLA:
+-- admin@demo.pl  -> Admin123!
+-- barber@demo.pl -> Barber123!
+-- client1..10    -> Client123!
