d81a8e6b4e56a2445028a0dcbcd0d849b8901422
MaciejPrzystarz <maciej.przystarz7@gmail.com>
2026-01-20 17:52:19 +0100

    Dodanie głównej funkcjonalności security-spring

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
+
     String home() {
-        return "index";
+        return "home";
     }
 }
