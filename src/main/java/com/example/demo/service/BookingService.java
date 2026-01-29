5c0e37dcfb22a64019da8cefa885b7c26a98d49a
MaciejPrzystarz <maciej.przystarz7@gmail.com>
2026-01-29 18:23:06 +0100

    Dodanie encji appointment oraz Klasy BookingService

---
+++ src/main/java/com/example/demo/service/BookingService.java
@@ -0,0 +1,69 @@
+package com.example.demo.service;
+
+import com.example.demo.model.AppointmentStatus;
+import com.example.demo.model.User;
+import com.example.demo.repository.AppointmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookingService {
+
+    private final AppointmentRepository appointmentRepository;
+
+    public BookingService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public List<LocalTime> getAvailableSlotsForTheWholeDay(User barber, LocalDate date) {
+
+        // 1. Ustawienie pracy 8:00-18:00
+        LocalTime workStart = LocalTime.of(8, 0);
+        LocalTime workEnd = LocalTime.of(18, 0);
+        int slotMinutes = 60;
+        LocalTime time;
+
+        // 2. Wygenerowanie wszysktich slotow
+        List<LocalTime> allSlots = new ArrayList<>();
+        for (time = workStart; // time = 8:00
+             time.isBefore(workEnd); // dopoki nie będzie 18:00
+             time = time.plusMinutes(slotMinutes)) // 1 slot = 60min na poczatek przynajmniej wiec tyle dodaje
+        {
+            allSlots.add(time); // czyli allSlots = {8:00, 9:00, 10:00..., 17:00}
+        }
+
+        // 3. Pobieram wszystkie zajete sloty
+        LocalDateTime from = date.atStartOfDay(); // aktualny dzień np. 2026-01-29 00:00
+        LocalDateTime to = date.plusDays(1).atStartOfDay(); // 2026-01-30 00:00 - czyli biore przedzial 24 godzin calego dnia
+        List<LocalTime> takenSlots = appointmentRepository.findByBarberAndStatusAndStartTimeBetween
+                        (barber, AppointmentStatus.BOOKED, from, to)
+                .stream()
+                .map(appointment -> appointment.getStartTime().toLocalTime())
+                .toList();
+
+        allSlots.removeAll(takenSlots);
+        return allSlots;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,21 @@
--- admin@demo.pl admin123 --
+-- admin1@demo.pl admin123 --
 INSERT INTO users (id, full_name, phone_number, email, password, role)
-VALUES (1, 'Admin Maciej', '234 567 891', 'admin@demo.pl',
+VALUES (1, 'Admin Maciej', '234 567 891', 'admin1@demo.pl',
         '$2b$10$415NvDFT7GwNFEL7Xgqhm.UcWlSl5U0Hv..SqZ7BRkTUs77lw19YO', 'ADMIN');
 
--- barber@demo.pl barber123 --
+-- barber1@demo.pl barber123 --
 INSERT INTO users (id, full_name, phone_number, email, password, role)
-VALUES (2, 'Barber Krzysztof', '123 456 789', 'barber@demo.pl', '$2b$10$zHyrkS13NMJ.T818GhMgmu/M2TsDh9yMEU4ChU5MeEl5ltQaKZnJy', 'BARBER');
+VALUES (2, 'Barber Krzysztof', '123 456 789', 'barber1@demo.pl', '$2b$10$zHyrkS13NMJ.T818GhMgmu/M2TsDh9yMEU4ChU5MeEl5ltQaKZnJy', 'BARBER');
 
--- client@demo.pl client123 --
+-- client1@demo.pl client123 --
 INSERT INTO users (id, full_name, phone_number, email, password, role) VALUES
-    (3, 'Client Kowalski', '345 678 912', 'client@demo.pl', '$2b$10$W0Au1b3AIBRDh7qydDusUuAlzUDCSYwWUlbsxrlOjV4zKy0ldIHUW', 'CLIENT');
+    (3, 'Client First', '345 678 912', 'client1@demo.pl', '$2b$10$W0Au1b3AIBRDh7qydDusUuAlzUDCSYwWUlbsxrlOjV4zKy0ldIHUW', 'CLIENT');
+
+-- client2@demo.pl client123 --
+INSERT INTO users (id, full_name, phone_number, email, password, role) VALUES
+    (4, 'Client Second', '456 789 123', 'client2@demo.pl', '$2b$10$W0Au1b3AIBRDh7qydDusUuAlzUDCSYwWUlbsxrlOjV4zKy0ldIHUW', 'CLIENT');
+
+-- client3@demo.pl client123 --
+INSERT INTO users (id, full_name, phone_number, email, password, role) VALUES
+    (5, 'Client Third', '567 891 234', 'client3@demo.pl', '$2b$10$W0Au1b3AIBRDh7qydDusUuAlzUDCSYwWUlbsxrlOjV4zKy0ldIHUW', 'CLIENT');
+
