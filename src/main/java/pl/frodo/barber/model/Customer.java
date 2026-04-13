7d5628ec215724bccf7dafbc2fa54137b75c5888
MaciejPrzystarz <maciej.przystarz7@gmail.com>
2026-04-13 19:24:44 +0200

    Fix CI pipeline issues

--- src/main/java/pl/frodo/barber/model/Customer.java
+++ src/main/java/pl/frodo/barber/model/Customer.java
@@ -1,6 +1,11 @@
 package pl.frodo.barber.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/pl/frodo/barber/repository/CustomerRepository.java
+++ src/main/java/pl/frodo/barber/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package pl.frodo.barber.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.frodo.barber.model.Customer;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
